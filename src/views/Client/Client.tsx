02e1ef728718ed053746bfb45ba366e43d993123
alesandi22 <alesf22@gmail.com>
2022-03-17 16:51:46 -0600

    single client page

--- src/views/Client/Client.tsx
+++ src/views/Client/Client.tsx
@@ -1,15 +1,60 @@
-import React from 'react';
+import moment from 'moment';
+import React, { useEffect, useRef, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import './Client.scss';
 
+import { format } from '../../services/daysLeft';
+
+
+function ClientBox(props: any) {
+
+  const [daysLeft, setDaysLeft] = useState<string>('');
+
+  useEffect(() => {
+    format(setDaysLeft,props);
+  }, [])
+
+  return(
+    <div className='client'>
+    <h3 className='name'>{ props.name }</h3>
+    <h4 className='days-left'>Days left: { daysLeft }</h4>
+    <h4 className='last-update-date'>Last update: { moment(props.date).format("YYYY/MM/DD") }</h4>
+    <h4 className='last-update-payment'>Last payment: ₡{ props.payment }</h4>
+</div>
+  );
+}
+
 export default function Client() {
+  
+  interface clientType {name: string, id: number, date: Date, payment: number}
+
+  const [ singleClient, setSingleClient ] = useState<JSX.Element>(<ClientBox />);
+  const clients = useRef<Array<object>>([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    new Promise( async (res: any) => {
+       const response: Response = await fetch('http://localhost:4000/clients');
+       clients.current = await response.json();
+       return res(clients);
+
+     }).then((clients: any) => {
+      clients.current = clients.current.filter((client: clientType) => {
+        return client.id.toString() === id;
+     })
+
+     setSingleClient(clients.current.map((client: any) => {
+       return <ClientBox key={client.id} name={client.name} date={client.date} payment={client.payment}/>
+     }))
+
+     
+    })
+   }, []);
+
   return (
     <div className='client-container'>
-      <div className='client'>
-          <h3 className='name'>Daniel-Benoît Thibault</h3>
-          <h4 className='days-left'>Days left: 20 days</h4>
-          <h4 className='last-update-date'>Last update: 10/02/2022</h4>
-          <h4 className='last-update-payment'>Last payment: ₡10.000</h4>
-      </div>
+      { singleClient }
     </div>
   )
 }
